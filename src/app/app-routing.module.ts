732c19f54e131e23994aa9dc0edbf646e03811cd
smit-patel-bacancy <smit.patel@bacancy.com>
2024-06-17 10:59:23 +0530

    Capacitor added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { Test1Component } from './test1/test1.component';
+import { Test2Component } from './test2/test2.component';
+
+const routes: Routes = [
+  { path: 'test1', component: Test1Component },
+  { path: 'test2', component: Test2Component },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
