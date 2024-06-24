01b95a41ed478ab9f5cf4040749677d7d12be29d
smit-patel-bacancy <smit.patel@bacancy.com>
2024-06-24 10:39:15 +0530

    @capacitor/device added and used

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Device, DeviceInfo } from '@capacitor/device';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tCapacitorDemo';
+  logDeviceInfo: any;
+  logDeviceId: any;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.logDeviceInfo = Device.getId();
+    console.warn('this.logDeviceInfo: ', this.logDeviceInfo);
+    this.logDeviceInfo.then(() => {
+      this.logDeviceId = this.logDeviceInfo.__zone_symbol__value.identifier;
+      console.warn('this.logDeviceId: ', this.logDeviceId);
+    });
+  }
+
 }
